29fab94b9a69694e7cde66cd5b8a07a996b1f451
Luiz <luizdavifreitas31@gmail.com>
2026-05-22 22:24:47 -0400

    Criação da classe 'no' classe 'árvore', com métodos inserir e buscar

---
+++ arvore.py
@@ -0,0 +1,41 @@
+import no
+
+
+class arvore:
+    def __init__(self):
+        self.raiz = None
+
+    def busca(self, raiz_pai, chave):
+        if raiz_pai == None:
+            return None
+        if raiz_pai.chave == chave:
+            return raiz_pai
+
+        atual = raiz_pai.filho
+
+        while atual:
+            resp = self.busca(atual, chave)
+            if resp:
+                return resp
+            atual = atual.irmao
+
+        return None
+
+    def insere(self, raiz, nova_chave, chave_pai):
+
+        pai = self.busca(raiz, chave_pai)
+        if pai is None:
+            return False
+
+        filho = no(nova_chave)
+
+        atual_i = pai.prim_filho
+
+        if atual_i is None:
+            pai.prim_filho = filho
+        else:
+            while atual_i.irmao:
+                atual_i = atual_i.irmao
+            atual_i.irmao = filho
+
+        return True
---
+++ no.py
@@ -0,0 +1,5 @@
+class no:
+    def __init__(self, chave):
+        self.chave = chave
+        self.esq = None
+        self.dir = None
